fb49c481e2a7ff1e9b42b3ebaff4db53e3f1178a
jalvarez00 <jalvarez@orienteed.com>
2023-04-13 16:47:27 +0200

    update storeConfig resolver

--- src/resolvers/storeConfig/GetStoreConfig/getStoreConfig.gql.ts
+++ src/resolvers/storeConfig/GetStoreConfig/getStoreConfig.gql.ts
@@ -1,9 +1,60 @@
 import { gql } from '@apollo/client';
 
-export const GET_STORE_CONFIG = gql`
-    # Your query here
+export const GET_STORE_CONFIG_CE = gql`
+    query GetStoreConfig {
+        storeConfig {
+            store_code
+            product_url_suffix
+            magento_wishlist_general_is_enabled
+            configurable_thumbnail_source
+            category_url_suffix
+            locale
+            grid_per_page
+            store_name
+            bank_transfer {
+                instructions
+            }
+            allow_order
+            contact_enabled
+            copyright
+            is_required_login
+            newsletter_enabled
+            root_category_uid
+            store_group_name
+        }
+    }
+`;
+
+export const GET_STORE_CONFIG_EE = gql`
+    query GetStoreConfig {
+        storeConfig {
+            store_code
+            product_url_suffix
+            magento_wishlist_general_is_enabled
+            configurable_thumbnail_source
+            category_url_suffix
+            locale
+            grid_per_page
+            store_name
+            bank_transfer {
+                instructions
+            }
+            allow_order
+            contact_enabled
+            copyright
+            is_required_login
+            newsletter_enabled
+            root_category_uid
+            store_group_name
+            allow_gift_receipt
+            allow_printed_card
+            enable_multiple_wishlists
+            maximum_number_of_wishlists
+        }
+    }
 `;
 
 export default {
-    getStoreConfigQuery: GET_STORE_CONFIG
+    getStoreConfigCEQuery: GET_STORE_CONFIG_CE,
+    getStoreConfigEEQuery: GET_STORE_CONFIG_EE
 };
--- src/resolvers/storeConfig/GetStoreConfig/getStoreConfig.ts
+++ src/resolvers/storeConfig/GetStoreConfig/getStoreConfig.ts
@@ -1,9 +1,19 @@
 import { ClientProps } from 'src';
+import DEFAULT_OPERATIONS from './getStoreConfig.gql';
 
 const GetStoreConfig = (clientProps: ClientProps) => () => {
-    // Look docs for more info about how to fill this function
+    const { useQuery, mergeOperations, backendEdition } = clientProps;
+    const operations = mergeOperations(DEFAULT_OPERATIONS);
 
-    return { data: {}, loading: false, error: undefined };
+    const { getStoreConfigCEQuery, getStoreConfigEEQuery } = operations;
+    const getStoreConfigQuery = backendEdition === 'EE' ? getStoreConfigEEQuery : getStoreConfigCEQuery;
+
+    const { data, refetch } = useQuery(getStoreConfigQuery, {
+        fetchPolicy: 'cache-and-network',
+        nextFetchPolicy: 'cache-first'
+    });
+
+    return { data, refetch };
 };
 
 export default GetStoreConfig;

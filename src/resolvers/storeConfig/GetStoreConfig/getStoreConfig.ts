e29b30403dfed6b26a78630e75894b84ce058826
jalvarez00 <jalvarez@orienteed.com>
2023-04-17 09:07:41 +0200

    update getCurrencyData resolver

--- src/resolvers/storeConfig/GetStoreConfig/getStoreConfig.ts
+++ src/resolvers/storeConfig/GetStoreConfig/getStoreConfig.ts
@@ -4,17 +4,18 @@ import DEFAULT_OPERATIONS from './getStoreConfig.gql';
 
 const GetStoreConfig = (clientProps: ClientProps) => () => {
     const { useQuery, mergeOperations, backendEdition } = clientProps;
-    const operations = mergeOperations(DEFAULT_OPERATIONS);
 
+    const operations = mergeOperations(DEFAULT_OPERATIONS);
     const { getStoreConfigCEQuery, getStoreConfigEEQuery } = operations;
+
     const getStoreConfigQuery = backendEdition === 'EE' ? getStoreConfigEEQuery : getStoreConfigCEQuery;
 
-    const { data, refetch } = useQuery(getStoreConfigQuery, {
+    const { data, loading, error, refetch } = useQuery(getStoreConfigQuery, {
         fetchPolicy: 'cache-and-network',
         nextFetchPolicy: 'cache-first'
     });
 
-    return { data, refetch };
+    return { data, loading, error, refetch };
 };
 
 export default GetStoreConfig;

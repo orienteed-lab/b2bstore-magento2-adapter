18338a79d4e1a5887ff9d672b7cea4e0187d459c
jalvarez00 <jalvarez@orienteed.com>
2023-04-18 10:56:20 +0200

    update getCategory resolver

--- src/resolvers/categories/GetCategory/getCategory.gql.ts
+++ src/resolvers/categories/GetCategory/getCategory.gql.ts
@@ -1,9 +1,28 @@
 import { gql } from '@apollo/client';
 
-export const GET_CATEGORY = gql`
-    # Your query here
-`;
+import { CategoryFragment, ProductsFragment } from './categoryFragments.gql';
 
+export const GET_CATEGORY = gql`
+    query GetCategory(
+        $id: String!
+        $pageSize: Int!
+        $currentPage: Int!
+        $filters: ProductAttributeFilterInput!
+        $sort: ProductAttributeSortInput
+    ) {
+        categories(filters: { category_uid: { in: [$id] } }) {
+            items {
+                uid
+                ...CategoryFragment
+            }
+        }
+        products(pageSize: $pageSize, currentPage: $currentPage, filter: $filters, sort: $sort) {
+            ...ProductsFragment
+        }
+    }
+    ${CategoryFragment}
+    ${ProductsFragment}
+`;
 export default {
     getCategoryQuery: GET_CATEGORY
 };

c8537620864180d9aff67437900147a119610d5d
jalvarez00 <jalvarez@orienteed.com>
2023-04-20 09:24:51 +0200

    update getCmsDynamicBlocks resolver

--- src/resolvers/cmsBlock/GetCmsDynamicBlocks/getCmsDynamicBlocks.gql.ts
+++ src/resolvers/cmsBlock/GetCmsDynamicBlocks/getCmsDynamicBlocks.gql.ts
@@ -1,7 +1,23 @@
 import { gql } from '@apollo/client';
 
 export const GET_CMS_DYNAMIC_BLOCKS = gql`
-    # Your query here
+    query GetCmsDynamicBlocks(
+        $cartId: String
+        $productId: ID
+        $type: DynamicBlockTypeEnum!
+        $locations: [DynamicBlockLocationEnum]
+        $uids: [ID]!
+    ) {
+        dynamicBlocks(input: { cart_id: $cartId, product_uid: $productId, dynamic_block_uids: $uids, locations: $locations, type: $type }) {
+            items {
+                content {
+                    html
+                }
+                uid
+            }
+            salesRulesData @client
+        }
+    }
 `;
 
 export default {

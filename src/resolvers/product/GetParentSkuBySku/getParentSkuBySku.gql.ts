12e89821c82597ced149325716a2418caba4c269
dggorienteed <dgonzalez@orienteed.com>
2023-05-09 10:18:39 +0200

    Update getParentSkuBySku resolver

--- src/resolvers/product/GetParentSkuBySku/getParentSkuBySku.gql.ts
+++ src/resolvers/product/GetParentSkuBySku/getParentSkuBySku.gql.ts
@@ -1,7 +1,14 @@
 import { gql } from '@apollo/client';
 
 export const GET_PARENT_SKU_BY_SKU = gql`
-    # Your query here
+    query GetParentSkuBySku($sku: String) {
+        products(search: $sku, filter: { sku: { eq: $sku } }) {
+            items {
+                orParentSku
+                uid
+            }
+        }
+    }
 `;
 
 export default {

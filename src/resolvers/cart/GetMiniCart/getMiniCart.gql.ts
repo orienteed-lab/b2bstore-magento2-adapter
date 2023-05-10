09e4efe68b9a09514331cc2f5f96e87c85ec71c2
dggorienteed <dgonzalez@orienteed.com>
2023-05-10 17:06:52 +0200

    update getMiniCart resolver

--- src/resolvers/cart/GetMiniCart/getMiniCart.gql.ts
+++ src/resolvers/cart/GetMiniCart/getMiniCart.gql.ts
@@ -1,7 +1,14 @@
 import { gql } from '@apollo/client';
+import { MiniCartFragment } from '../../fragments/miniCartFragments.gql'
 
 export const GET_MINI_CART = gql`
-    # Your query here
+    query GetMiniCart($cartId: String!) {
+        cart(cart_id: $cartId) {
+            id
+            ...MiniCartFragment
+        }
+    }
+    ${MiniCartFragment}
 `;
 
 export default {
--- src/resolvers/cart/GetMiniCart/getMiniCart.ts
+++ src/resolvers/cart/GetMiniCart/getMiniCart.ts
@@ -1,10 +1,22 @@
 import { ClientProps } from 'src';
 import { GetMiniCartQueryVariables } from '@schema';
 
-const GetMiniCart = (clientProps: ClientProps) => (resolverProps: GetMiniCartQueryVariables) => {
-    // Look docs for more info about how to fill this function
+import DEFAULT_OPERATIONS from './getMiniCart.gql';
 
-    return { data: {}, loading: false, error: undefined };
+const GetMiniCart = (clientProps: ClientProps) => (resolverProps: GetMiniCartQueryVariables) => {
+    const { mergeOperations, useQuery } = clientProps;
+    const { cartId } = resolverProps;
+
+    const { getMiniCartQuery } = mergeOperations(DEFAULT_OPERATIONS);
+    const { data, loading } = useQuery(getMiniCartQuery, {
+        fetchPolicy: 'cache-and-network',
+        nextFetchPolicy: 'cache-first',
+        variables: { cartId },
+        skip: !cartId,
+        errorPolicy: 'all'
+    });
+
+    return { data, loading };
 };
 
 export default GetMiniCart;

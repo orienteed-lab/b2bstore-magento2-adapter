3a668314f70854b34ca7eb08fb29cc1f1c5ae322
dggorienteed <dgonzalez@orienteed.com>
2023-05-12 09:25:16 +0200

    Update setBillingAddress resolver

--- src/resolvers/cart/billing/SetBillingAddress/setBillingAddress.ts
+++ src/resolvers/cart/billing/SetBillingAddress/setBillingAddress.ts
@@ -1,10 +1,17 @@
 import { ClientProps } from 'src';
 import { SetBillingAddressMutationVariables } from '@schema';
 
-const SetBillingAddress = (clientProps: ClientProps) => (resolverProps: SetBillingAddressMutationVariables) => {
-    // Look docs for more info about how to fill this function
+import DEFAULT_OPERATIONS from './setBillingAddress.gql';
 
-    return { data: {}, loading: false, error: undefined };
+const SetBillingAddress = (clientProps: ClientProps) => (resolverProps: SetBillingAddressMutationVariables) => {
+    const { mergeOperations, useMutation, backendEdition } = clientProps;
+
+    const { setBillingAddressMutationCE, setBillingAddressMutationEE } = mergeOperations(DEFAULT_OPERATIONS);
+    const setBillingAddressMutation = backendEdition === 'EE' ? setBillingAddressMutationEE : setBillingAddressMutationCE;
+
+    const [updateBillingAddress, { error, called, loading }] = useMutation(setBillingAddressMutation);
+
+    return { updateBillingAddress, called, loading, error };
 };
 
 export default SetBillingAddress;

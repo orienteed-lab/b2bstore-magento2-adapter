1223bc6b36fe389aa74581b4bfa36ea85c4b390a
dggorienteed <dgonzalez@orienteed.com>
2023-05-16 10:10:59 +0200

    Update removeItemFromCart resolver

--- src/resolvers/cart/RemoveItemFromCart/removeItemFromCart.ts
+++ src/resolvers/cart/RemoveItemFromCart/removeItemFromCart.ts
@@ -1,10 +1,17 @@
 import { ClientProps } from 'src';
 import { RemoveItemFromCartMutationVariables } from '@schema';
 
-const RemoveItemFromCart = (clientProps: ClientProps) => (resolverProps: RemoveItemFromCartMutationVariables) => {
-    // Look docs for more info about how to fill this function
+import DEFAULT_OPERATIONS from './removeItemFromCart.gql';
 
-    return { data: {}, loading: false, error: undefined };
+const RemoveItemFromCart = (clientProps: ClientProps) => (resolverProps: RemoveItemFromCartMutationVariables) => {
+    const { mergeOperations, useMutation, backendEdition } = clientProps;
+
+    const { removeItemFromCartMutationCE, removeItemFromCartMutationEE } = mergeOperations(DEFAULT_OPERATIONS);
+    const removeItemFromCartMutation = backendEdition === 'EE' ? removeItemFromCartMutationEE : removeItemFromCartMutationCE;
+
+    const [removeItem, { loading, called, error }] = useMutation(removeItemFromCartMutation);
+
+    return { removeItem, called, loading, error };
 };
 
 export default RemoveItemFromCart;

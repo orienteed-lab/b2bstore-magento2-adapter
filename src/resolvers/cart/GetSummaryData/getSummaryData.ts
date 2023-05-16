5b3b0e80ba0b3e212b5a079454a75ce4da27a82d
dggorienteed <dgonzalez@orienteed.com>
2023-05-16 08:41:01 +0200

    Update getSummaryData resolver

--- src/resolvers/cart/GetSummaryData/getSummaryData.ts
+++ src/resolvers/cart/GetSummaryData/getSummaryData.ts
@@ -1,10 +1,20 @@
 import { ClientProps } from 'src';
 import { GetSummaryDataQueryVariables } from '@schema';
 
-const GetSummaryData = (clientProps: ClientProps) => (resolverProps: GetSummaryDataQueryVariables) => {
-    // Look docs for more info about how to fill this function
+import DEFAULT_OPERATIONS from './getSummaryData.gql';
 
-    return { data: {}, loading: false, error: undefined };
+const GetSummaryData = (clientProps: ClientProps) => (resolverProps: GetSummaryDataQueryVariables) => {
+    const { mergeOperations, useQuery } = clientProps;
+    const { cartId } = resolverProps;
+
+    const { getSummaryDataQuery } = mergeOperations(DEFAULT_OPERATIONS);
+
+    const { data, loading } = useQuery(getSummaryDataQuery, {
+        skip: !cartId,
+        variables: { cartId }
+    });
+
+    return { data, loading };
 };
 
 export default GetSummaryData;

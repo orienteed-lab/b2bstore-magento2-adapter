71e1f7827e8c4ba83866a6a04dc598b180ded9b7
dggorienteed <dgonzalez@orienteed.com>
2023-05-18 08:50:02 +0200

    Update updateCartItems resolver

--- src/resolvers/cart/UpdateCartItems/updateCartItems.gql.ts
+++ src/resolvers/cart/UpdateCartItems/updateCartItems.gql.ts
@@ -1,9 +1,45 @@
 import { gql } from '@apollo/client';
 
-export const UPDATE_CART_ITEMS = gql`
-    # Your query here
+import { CartPageFragmentCE, CartPageFragmentEE } from '../../fragments/cartPageFragments.gql';
+import { AvailableShippingMethodsCartFragment } from '../../fragments/shippingMethodsFragments.gql';
+
+export const UPDATE_CART_ITEMS_CE = gql`
+    mutation UpdateCartItems($cartId: String!, $itemId: ID!, $quantity: Float!) {
+        updateCartItems(input: { cart_id: $cartId, cart_items: [{ cart_item_uid: $itemId, quantity: $quantity }] }) {
+            cart {
+                id
+                available_payment_methods {
+                    code
+                    title
+                }
+                ...CartPageFragment
+                ...AvailableShippingMethodsCartFragment
+            }
+        }
+    }
+    ${CartPageFragmentCE}
+    ${AvailableShippingMethodsCartFragment}
+`;
+
+export const UPDATE_CART_ITEMS_EE = gql`
+    mutation UpdateCartItems($cartId: String!, $itemId: ID!, $quantity: Float!) {
+        updateCartItems(input: { cart_id: $cartId, cart_items: [{ cart_item_uid: $itemId, quantity: $quantity }] }) {
+            cart {
+                id
+                available_payment_methods {
+                    code
+                    title
+                }
+                ...CartPageFragment
+                ...AvailableShippingMethodsCartFragment
+            }
+        }
+    }
+    ${CartPageFragmentEE}
+    ${AvailableShippingMethodsCartFragment}
 `;
 
 export default {
-    updateCartItemsMutation: UPDATE_CART_ITEMS
+    updateCartItemsMutationCE: UPDATE_CART_ITEMS_CE,
+    updateCartItemsMutationEE: UPDATE_CART_ITEMS_EE
 };

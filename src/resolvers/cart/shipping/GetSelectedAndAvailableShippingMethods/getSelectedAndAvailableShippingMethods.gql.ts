7fd912b444df367b05a248f673f94fc38702b77d
dggorienteed <dgonzalez@orienteed.com>
2023-05-23 08:49:23 +0200

    Update getSelectedAndAvailableShippingMethods

--- src/resolvers/cart/shipping/GetSelectedAndAvailableShippingMethods/getSelectedAndAvailableShippingMethods.gql.ts
+++ src/resolvers/cart/shipping/GetSelectedAndAvailableShippingMethods/getSelectedAndAvailableShippingMethods.gql.ts
@@ -1,7 +1,20 @@
 import { gql } from '@apollo/client';
 
+import { AvailableShippingMethodsCheckoutFragment, SelectedShippingMethodCheckoutFragment } from '../../../fragments/shippingMethodsFragments.gql';
+import { ShippingInformationFragment } from '../../../fragments/shippingInformationFragments.gql';
+
 export const GET_SELECTED_AND_AVAILABLE_SHIPPING_METHODS = gql`
-    # Your query here
+    query GetSelectedAndAvailableShippingMethods($cartId: String!) {
+        cart(cart_id: $cartId) {
+            id
+            ...AvailableShippingMethodsCheckoutFragment
+            ...SelectedShippingMethodCheckoutFragment
+            ...ShippingInformationFragment
+        }
+    }
+    ${AvailableShippingMethodsCheckoutFragment}
+    ${SelectedShippingMethodCheckoutFragment}
+    ${ShippingInformationFragment}
 `;
 
 export default {

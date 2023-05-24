f9d9d9d19cfc9a56d1ea5aff39059ef70cb34a3a
dggorienteed <dgonzalez@orienteed.com>
2023-05-24 08:59:12 +0200

    Update setShippingMethod resolver

--- src/resolvers/cart/shipping/SetShippingMethod/setShippingMethod.gql.ts
+++ src/resolvers/cart/shipping/SetShippingMethod/setShippingMethod.gql.ts
@@ -1,9 +1,56 @@
 import { gql } from '@apollo/client';
 
-export const SET_SHIPPING_METHOD = gql`
-    # Your query here
+import { AvailableShippingMethodsCheckoutFragment, SelectedShippingMethodCheckoutFragment } from '../../../fragments/shippingMethodsFragments.gql';
+import { PriceSummaryFragmentCE, PriceSummaryFragmentEE } from '../../../fragments/priceSummaryFragments.gql';
+import { ShippingInformationFragment } from '../../../fragments/shippingInformationFragments.gql';
+
+export const SET_SHIPPING_METHOD_CE = gql`
+    mutation SetShippingMethod($cartId: String!, $shippingMethod: ShippingMethodInput!) {
+        setShippingMethodsOnCart(input: { cart_id: $cartId, shipping_methods: [$shippingMethod] }) {
+            cart {
+                id
+                # If this mutation causes "free" to become available we need to know.
+                available_payment_methods {
+                    code
+                    title
+                }
+                ...AvailableShippingMethodsCheckoutFragment
+                ...PriceSummaryFragment
+                ...SelectedShippingMethodCheckoutFragment
+                ...ShippingInformationFragment
+            }
+        }
+    }
+    ${AvailableShippingMethodsCheckoutFragment}
+    ${PriceSummaryFragmentCE}
+    ${SelectedShippingMethodCheckoutFragment}
+    ${ShippingInformationFragment}
+`;
+
+export const SET_SHIPPING_METHOD_EE = gql`
+    mutation SetShippingMethod($cartId: String!, $shippingMethod: ShippingMethodInput!) {
+        setShippingMethodsOnCart(input: { cart_id: $cartId, shipping_methods: [$shippingMethod] }) {
+            cart {
+                id
+                # If this mutation causes "free" to become available we need to know.
+                available_payment_methods {
+                    code
+                    title
+                }
+                ...AvailableShippingMethodsCheckoutFragment
+                ...PriceSummaryFragment
+                ...SelectedShippingMethodCheckoutFragment
+                ...ShippingInformationFragment
+            }
+        }
+    }
+    ${AvailableShippingMethodsCheckoutFragment}
+    ${PriceSummaryFragmentEE}
+    ${SelectedShippingMethodCheckoutFragment}
+    ${ShippingInformationFragment}
 `;
 
 export default {
-    setShippingMethodMutation: SET_SHIPPING_METHOD
+    setShippingMethodMutationCE: SET_SHIPPING_METHOD_CE,
+    setShippingMethodMutationEE: SET_SHIPPING_METHOD_EE
 };

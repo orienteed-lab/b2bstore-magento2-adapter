8a97a2df39b4d91f87073dc0e4696f795dbf7671
dggorienteed <dgonzalez@orienteed.com>
2023-05-24 08:39:24 +0200

    Update setShippingAddress resolver

--- src/resolvers/cart/shipping/SetShippingAddress/setShippingAddress.gql.ts
+++ src/resolvers/cart/shipping/SetShippingAddress/setShippingAddress.gql.ts
@@ -1,9 +1,42 @@
 import { gql } from '@apollo/client';
 
-export const SET_SHIPPING_ADDRESS = gql`
-    # Your query here
+import { CartPageFragmentCE, CartPageFragmentEE } from '../../../fragments/cartPageFragments.gql';
+import { ShippingMethodsCartFragment } from '../../../fragments/shippingMethodsFragments.gql';
+import { ShippingInformationFragment } from '../../../fragments/shippingInformationFragments.gql';
+
+export const SET_SHIPPING_ADDRESS_CE = gql`
+    mutation SetShippingAddress($cartId: String!, $address: CartAddressInput!) {
+        setShippingAddressesOnCart(input: { cart_id: $cartId, shipping_addresses: [{ address: $address }] }) {
+            cart {
+                id
+                ...CartPageFragment
+                ...ShippingMethodsCartFragment
+                ...ShippingInformationFragment
+            }
+        }
+    }
+    ${CartPageFragmentCE}
+    ${ShippingMethodsCartFragment}
+    ${ShippingInformationFragment}
+`;
+
+export const SET_SHIPPING_ADDRESS_EE = gql`
+    mutation SetShippingAddress($cartId: String!, $address: CartAddressInput!) {
+        setShippingAddressesOnCart(input: { cart_id: $cartId, shipping_addresses: [{ address: $address }] }) {
+            cart {
+                id
+                ...CartPageFragment
+                ...ShippingMethodsCartFragment
+                ...ShippingInformationFragment
+            }
+        }
+    }
+    ${CartPageFragmentEE}
+    ${ShippingMethodsCartFragment}
+    ${ShippingInformationFragment}
 `;
 
 export default {
-    setShippingAddressMutation: SET_SHIPPING_ADDRESS
+    setShippingAddressMutationCE: SET_SHIPPING_ADDRESS_CE,
+    setShippingAddressMutationEE: SET_SHIPPING_ADDRESS_EE
 };

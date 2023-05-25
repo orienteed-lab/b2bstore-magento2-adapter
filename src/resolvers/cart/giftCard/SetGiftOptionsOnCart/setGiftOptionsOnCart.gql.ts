1366691e3afff77c14009e705ffa1ca33d0ef5e6
dggorienteed <dgonzalez@orienteed.com>
2023-05-25 09:15:37 +0200

    Update setGiftOptionsOnCart resolver

--- src/resolvers/cart/giftCard/SetGiftOptionsOnCart/setGiftOptionsOnCart.gql.ts
+++ src/resolvers/cart/giftCard/SetGiftOptionsOnCart/setGiftOptionsOnCart.gql.ts
@@ -1,7 +1,32 @@
 import { gql } from '@apollo/client';
 
+import { CartPageFragmentEE } from '../../../fragments/cartPageFragments.gql';
+import { GiftOptionsFragmentEE } from '../../../fragments/giftOptionsFragments.gql';
+
 export const SET_GIFT_OPTIONS_ON_CART = gql`
-    # Your query here
+    mutation SetGiftOptionsOnCart(
+        $cartId: String!
+        $giftMessage: GiftMessageInput
+        $giftReceiptIncluded: Boolean!
+        $printedCardIncluded: Boolean!
+    ) {
+        setGiftOptionsOnCart(
+            input: {
+                cart_id: $cartId
+                gift_message: $giftMessage
+                gift_receipt_included: $giftReceiptIncluded
+                printed_card_included: $printedCardIncluded
+            }
+        ) {
+            cart {
+                id
+                ...CartPageFragment
+                ...GiftOptionsFragment
+            }
+        }
+    }
+    ${CartPageFragmentEE}
+    ${GiftOptionsFragmentEE}
 `;
 
 export default {

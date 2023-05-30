21931e2328d07b685490d57b9aea4c22167b165b
dggorienteed <dgonzalez@orienteed.com>
2023-05-30 10:16:58 +0200

    Update getImageBySku resolver

--- src/resolvers/product/GetImageBySku/getImageBySku.gql.ts
+++ src/resolvers/product/GetImageBySku/getImageBySku.gql.ts
@@ -1,7 +1,16 @@
 import { gql } from '@apollo/client';
 
 export const GET_IMAGE_BY_SKU = gql`
-    # Your query here
+    query GetImageBySku($sku: String) {
+        products(search: $sku, filter: { sku: { eq: $sku } }) {
+            items {
+                uid
+                image {
+                    url
+                }
+            }
+        }
+    }
 `;
 
 export default {

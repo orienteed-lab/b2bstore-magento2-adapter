81495c8c798e709ecd4be1e45c8a2ff236c691d1
dggorienteed <dgonzalez@orienteed.com>
2023-06-01 10:17:50 +0200

    Update getProductThumbnailsByUrlKey resolver

--- src/resolvers/product/GetProductThumbnailsByUrlKey/getProductThumbnailsByUrlKey.gql.ts
+++ src/resolvers/product/GetProductThumbnailsByUrlKey/getProductThumbnailsByUrlKey.gql.ts
@@ -1,7 +1,31 @@
 import { gql } from '@apollo/client';
 
 export const GET_PRODUCT_THUMBNAILS_BY_URL_KEY = gql`
-    # Your query here
+    query GetProductThumbnailsByUrlKey($urlKeys: [String!]!) {
+        products(filter: { url_key: { in: $urlKeys } }) {
+            items {
+                uid
+                sku
+                thumbnail {
+                    label
+                    url
+                }
+                url_key
+                ... on ConfigurableProduct {
+                    variants {
+                        product {
+                            sku
+                            uid
+                            thumbnail {
+                                label
+                                url
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 `;
 
 export default {

a5937de25644af940e9aeb323b120486caa8bd21
dggorienteed <dgonzalez@orienteed.com>
2023-06-01 10:36:16 +0200

    Update updateConfigurableOptions resolver

--- src/resolvers/product/UpdateConfigurableOptions/updateConfigurableOptions.gql.ts
+++ src/resolvers/product/UpdateConfigurableOptions/updateConfigurableOptions.gql.ts
@@ -1,9 +1,66 @@
 import { gql } from '@apollo/client';
 
-export const UPDATE_CONFIGURABLE_OPTIONS = gql`
-    # Your query here
+import { CartPageFragmentCE, CartPageFragmentEE } from '../../fragments/cartPageFragments.gql';
+
+export const UPDATE_CONFIGURABLE_OPTIONS_CE = gql`
+    mutation UpdateConfigurableOptions(
+        $cartId: String!
+        $cartItemId: ID!
+        $parentSku: String!
+        $variantSku: String!
+        $quantity: Float!
+    ) {
+        addConfigurableProductsToCart(
+            input: {
+                cart_id: $cartId
+                cart_items: [{ data: { quantity: $quantity, sku: $variantSku }, parent_sku: $parentSku }]
+            }
+        ) {
+            cart {
+                id
+            }
+        }
+
+        removeItemFromCart(input: { cart_id: $cartId, cart_item_uid: $cartItemId }) {
+            cart {
+                id
+                ...CartPageFragment
+            }
+        }
+    }
+    ${CartPageFragmentCE}
+`;
+
+export const UPDATE_CONFIGURABLE_OPTIONS_EE = gql`
+    mutation UpdateConfigurableOptions(
+        $cartId: String!
+        $cartItemId: ID!
+        $parentSku: String!
+        $variantSku: String!
+        $quantity: Float!
+    ) {
+        addConfigurableProductsToCart(
+            input: {
+                cart_id: $cartId
+                cart_items: [{ data: { quantity: $quantity, sku: $variantSku }, parent_sku: $parentSku }]
+            }
+        ) {
+            cart {
+                id
+            }
+        }
+
+        removeItemFromCart(input: { cart_id: $cartId, cart_item_uid: $cartItemId }) {
+            cart {
+                id
+                ...CartPageFragment
+            }
+        }
+    }
+    ${CartPageFragmentEE}
 `;
 
 export default {
-    updateConfigurableOptionsMutation: UPDATE_CONFIGURABLE_OPTIONS
+    updateConfigurableOptionsMutationCE: UPDATE_CONFIGURABLE_OPTIONS_CE,
+    updateConfigurableOptionsMutationEE: UPDATE_CONFIGURABLE_OPTIONS_EE
 };

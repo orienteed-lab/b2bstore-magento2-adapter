efa492386a46c376c34098d9d3d1bfa409c05d78
dggorienteed <dgonzalez@orienteed.com>
2023-06-01 08:57:19 +0200

    Update getProductItemsFilteredByCategory resolver

--- src/resolvers/product/GetProductItemsFilteredByCategory/getProductItemsFilteredByCategory.gql.ts
+++ src/resolvers/product/GetProductItemsFilteredByCategory/getProductItemsFilteredByCategory.gql.ts
@@ -1,7 +1,18 @@
 import { gql } from '@apollo/client';
 
 export const GET_PRODUCT_ITEMS_FILTERED_BY_CATEGORY = gql`
-    # Your query here
+    query GetProductItemsFilteredByCategory($categoryIdFilter: FilterEqualTypeInput!) {
+        products(filter: { category_uid: $categoryIdFilter }) {
+            items {
+                id
+                uid
+                __typename
+                name
+                url_key
+                url_suffix
+            }
+        }
+    }
 `;
 
 export default {

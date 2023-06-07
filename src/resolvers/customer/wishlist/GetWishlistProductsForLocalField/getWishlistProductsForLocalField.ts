1c1de7887ddaf4fbdd07f645098b4fb772978ff3
dggorienteed <dgonzalez@orienteed.com>
2023-06-07 13:03:45 +0200

    Update getWishlistProductsForLocalField & add rule

--- src/resolvers/customer/wishlist/GetWishlistProductsForLocalField/getWishlistProductsForLocalField.ts
+++ src/resolvers/customer/wishlist/GetWishlistProductsForLocalField/getWishlistProductsForLocalField.ts
@@ -1,10 +1,62 @@
 import { ClientProps } from 'src';
 import { GetWishlistProductsForLocalFieldQueryVariables } from '@schema';
 
-const GetWishlistProductsForLocalField = (clientProps: ClientProps) => (resolverProps: GetWishlistProductsForLocalFieldQueryVariables) => {
-    // Look docs for more info about how to fill this function
+import DEFAULT_OPERATIONS from './getWishlistProductsForLocalField.gql';
+import WISHLIST_OPERATIONS from '../GetProductsInWishlists/getProductsInWishlists.gql';
 
-    return { data: {}, loading: false, error: undefined };
+interface GetWishlistProductsForLocalFieldProps extends GetWishlistProductsForLocalFieldQueryVariables {
+    customerWishlistProducts: any;
+    client: any;
+    setCurrentPage: any;
+    isSignedIn: boolean;
+}
+
+const GetWishlistProductsForLocalField = (clientProps: ClientProps) => (resolverProps: GetWishlistProductsForLocalFieldProps) => {
+    const { mergeOperations, useQuery } = clientProps;
+    const { currentPage, client, isSignedIn, customerWishlistProducts, setCurrentPage } = resolverProps;
+
+    const { getWishlistProductsForLocalFieldQuery, getProductsInWishlistsQuery } = mergeOperations(DEFAULT_OPERATIONS, WISHLIST_OPERATIONS);
+
+    useQuery(getWishlistProductsForLocalFieldQuery, {
+        fetchPolicy: 'cache-and-network',
+        onCompleted: (data: any) => {
+            const itemsToAdd = new Set();
+            const wishlists = data.customer.wishlists;
+            let shouldFetchMore = false;
+            wishlists.map((wishlist: any) => {
+                const items = wishlist.items_v2.items;
+                items.map((item: any) => {
+                    const sku = item.product.sku;
+                    if (!customerWishlistProducts.includes(sku)) {
+                        itemsToAdd.add(sku);
+                    }
+                });
+
+                const pageInfo = wishlist.items_v2.page_info;
+
+                if (pageInfo.total_pages > pageInfo.current_page) {
+                    shouldFetchMore = true;
+                }
+            });
+
+            if (itemsToAdd.size) {
+                client.writeQuery({
+                    query: getProductsInWishlistsQuery,
+                    data: {
+                        customerWishlistProducts: [...customerWishlistProducts, ...itemsToAdd]
+                    }
+                });
+            }
+
+            if (shouldFetchMore) {
+                setCurrentPage((current: any) => ++current);
+            }
+        },
+        skip: !isSignedIn,
+        variables: {
+            currentPage
+        }
+    });
 };
 
 export default GetWishlistProductsForLocalField;

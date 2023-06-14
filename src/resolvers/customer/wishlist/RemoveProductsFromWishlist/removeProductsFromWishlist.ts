7ddbff58448502b02b40f23054568505bb9d8aaf
dggorienteed <dgonzalez@orienteed.com>
2023-06-14 16:44:49 +0200

    Update removeProductsFromWishlist resolver

--- src/resolvers/customer/wishlist/RemoveProductsFromWishlist/removeProductsFromWishlist.ts
+++ src/resolvers/customer/wishlist/RemoveProductsFromWishlist/removeProductsFromWishlist.ts
@@ -1,10 +1,91 @@
 import { ClientProps } from 'src';
 import { RemoveProductsFromWishlistMutationVariables } from '@schema';
 
-const RemoveProductsFromWishlist = (clientProps: ClientProps) => (resolverProps: RemoveProductsFromWishlistMutationVariables) => {
-    // Look docs for more info about how to fill this function
+import DEFAULT_OPERATIONS from './removeProductsFromWishlist.gql';
 
-    return { data: {}, loading: false, error: undefined };
-};
+interface RemoveProductsFromWishlistProps extends RemoveProductsFromWishlistMutationVariables {
+    sku?: any;
+    item?: any;
+    isFromUse?: boolean;
+}
+
+const RemoveProductsFromWishlist =
+    (clientProps: ClientProps) =>
+    (resolverProps: RemoveProductsFromWishlistProps = { isFromUse: false, wishlistId: '', wishlistItemsId: '' }) => {
+        const { mergeOperations, backendEdition, useMutation } = clientProps;
+        const { wishlistId, wishlistItemsId, isFromUse } = resolverProps;
+
+        const { removeProductsFromWishlistMutationCE, removeProductsFromWishlistMutationEE } = mergeOperations(DEFAULT_OPERATIONS);
+        const removeProductsFromWishlistMutation =
+            backendEdition === 'EE' ? removeProductsFromWishlistMutationEE : removeProductsFromWishlistMutationCE;
+
+        if (isFromUse) {
+            const { sku, item } = resolverProps;
+            const [removeProductsFromWishlist] = useMutation(removeProductsFromWishlistMutation, {
+                update: (cache: any) => {
+                    // clean up for cache fav product on category page
+                    cache.modify({
+                        id: 'ROOT_QUERY',
+                        fields: {
+                            customerWishlistProducts: (cachedProducts: any) =>
+                                cachedProducts.filter((productSku: any) => productSku !== sku)
+                        }
+                    });
+
+                    cache.modify({
+                        id: `CustomerWishlist:${wishlistId}`,
+                        fields: {
+                            items_v2: (cachedItems: { items: string | any[] }, { readField, Remove }: any) => {
+                                for (let i = 0; i < cachedItems.items.length; i++) {
+                                    if (readField('id', item) === wishlistItemsId[0]) {
+                                        return Remove;
+                                    }
+                                }
+
+                                return cachedItems;
+                            }
+                        }
+                    });
+                },
+                variables: {
+                    wishlistId,
+                    wishlistItemsId
+                }
+            });
+
+            return { removeProductsFromWishlist };
+        } else {
+            const [removeProductsFromWishlist] = useMutation(removeProductsFromWishlistMutation, {
+                update: (cache: any) => {
+                    cache.modify({
+                        id: 'ROOT_QUERY',
+                        fields: {
+                            customerWishlistProducts: (cachedProducts: any) => {
+                                return cachedProducts.slice(0, 0);
+                            }
+                        }
+                    });
+
+                    cache.modify({
+                        id: `CustomerWishlist:${wishlistId}`,
+                        fields: {
+                            items_v2: (cachedItems: { items: string | any[] }, { Remove }: any) => {
+                                for (let i = 0; i < cachedItems.items.length; i++) {
+                                    return Remove;
+                                }
+                                return cachedItems;
+                            }
+                        }
+                    });
+                },
+                variables: {
+                    wishlistId,
+                    wishlistItemsId
+                }
+            });
+
+            return { removeProductsFromWishlist };
+        }
+    };
 
 export default RemoveProductsFromWishlist;

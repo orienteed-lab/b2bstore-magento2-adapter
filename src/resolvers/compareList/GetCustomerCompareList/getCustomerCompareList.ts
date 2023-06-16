bdee7b69c5c6db97d1839e289ba2e60c4f25f356
dggorienteed <dgonzalez@orienteed.com>
2023-06-16 12:19:54 +0200

    Update getCustomerCompareList resolver

--- src/resolvers/compareList/GetCustomerCompareList/getCustomerCompareList.ts
+++ src/resolvers/compareList/GetCustomerCompareList/getCustomerCompareList.ts
@@ -1,9 +1,18 @@
 import { ClientProps } from 'src';
 
-const GetCustomerCompareList = (clientProps: ClientProps) => () => {
-    // Look docs for more info about how to fill this function
+import DEFAULT_OPERATIONS from './getCustomerCompareList.gql';
 
-    return { data: {}, loading: false, error: undefined };
+const GetCustomerCompareList = (clientProps: ClientProps) => () => {
+    const { mergeOperations, useLazyQuery } = clientProps;
+
+    const { getCustomerCompareListQuery } = mergeOperations(DEFAULT_OPERATIONS);
+
+    const [getCustomerCompareList, { data, loading }] = useLazyQuery(getCustomerCompareListQuery, {
+        fetchPolicy: 'cache-and-network',
+        nextFetchPolicy: 'cache-first'
+    });
+
+    return { getCustomerCompareList, data, loading };
 };
 
 export default GetCustomerCompareList;

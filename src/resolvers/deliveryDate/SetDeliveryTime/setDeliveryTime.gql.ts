338f0fbfe097248139b1d0e15c4f4cf5f56f7854
dggorienteed <dgonzalez@orienteed.com>
2023-06-16 12:52:08 +0200

    Update setDeliveryTime resolver

--- src/resolvers/deliveryDate/SetDeliveryTime/setDeliveryTime.gql.ts
+++ src/resolvers/deliveryDate/SetDeliveryTime/setDeliveryTime.gql.ts
@@ -1,7 +1,9 @@
 import { gql } from '@apollo/client';
 
 export const SET_DELIVERY_TIME = gql`
-    # Your query here
+    mutation SetDeliveryTime($cart_id: String!, $mp_delivery_time: DeliveryTimeInput) {
+        MpDeliveryTime(cart_id: $cart_id, mp_delivery_time: $mp_delivery_time)
+    }
 `;
 
 export default {

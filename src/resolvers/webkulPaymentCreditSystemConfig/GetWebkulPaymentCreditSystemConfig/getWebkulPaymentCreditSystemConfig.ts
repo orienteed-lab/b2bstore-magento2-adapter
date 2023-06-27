b8a6f26b1be047882a00094d7ed59e3176f9539b
dggorienteed <dgonzalez@orienteed.com>
2023-06-27 15:49:54 +0200

    Update getWebkulPaymentCreditSystemConfig resolver

--- src/resolvers/webkulPaymentCreditSystemConfig/GetWebkulPaymentCreditSystemConfig/getWebkulPaymentCreditSystemConfig.ts
+++ src/resolvers/webkulPaymentCreditSystemConfig/GetWebkulPaymentCreditSystemConfig/getWebkulPaymentCreditSystemConfig.ts
@@ -1,9 +1,18 @@
 import { ClientProps } from 'src';
 
-const GetWebkulPaymentCreditSystemConfig = (clientProps: ClientProps) => () => {
-    // Look docs for more info about how to fill this function
+import DEFAULT_OPERATIONS from './getWebkulPaymentCreditSystemConfig.gql';
 
-    return { data: {}, loading: false, error: undefined };
+const GetWebkulPaymentCreditSystemConfig = (clientProps: ClientProps) => () => {
+    const { mergeOperations, useQuery } = clientProps;
+
+    const { getWebkulPaymentCreditSystemConfigQuery } = mergeOperations(DEFAULT_OPERATIONS);
+
+    const { data, loading } = useQuery(getWebkulPaymentCreditSystemConfigQuery, {
+        fetchPolicy: 'cache-and-network',
+        nextFetchPolicy: 'cache-first'
+    });
+
+    return { data, loading };
 };
 
 export default GetWebkulPaymentCreditSystemConfig;
